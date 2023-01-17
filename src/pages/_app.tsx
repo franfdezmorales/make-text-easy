94672c94a40151ebe9a02b2d54c50cfa9a096103
franfdezmorales <franfdezmorales@gmail.com>
2023-01-17 19:21:37 +0100

    feat(app): add translation provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '@components/Layout'
+import { NextIntlProvider } from 'next-intl';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextIntlProvider messages={pageProps.messages}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </NextIntlProvider>
+  )
 }
