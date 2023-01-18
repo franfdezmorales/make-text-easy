86892cf1ad3ce6e563d05442df894070218b3734
franfdezmorales <franfdezmorales@gmail.com>
2023-01-18 09:40:42 +0100

    feat(analytics): add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,18 @@ import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@components/Layout'
 import { NextIntlProvider } from 'next-intl';
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextIntlProvider messages={pageProps.messages}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </NextIntlProvider>
+    <>
+      <NextIntlProvider messages={pageProps.messages}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </NextIntlProvider>
+      <Analytics />
+    </>
+    
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,33 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Imagine that you can modify a text with a few simple instructions, now you can achieve it."
+          />
+          <meta property="og:site_name" content="maketexteasy.com" />
+          <meta
+            property="og:description"
+            content="Imagine that you can modify a text with a few simple instructions, now you can achieve it."
+          />
+          <meta property="og:title" content="Make Text Easy" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Make Text Easy" />
+          <meta
+            name="twitter:description"
+            content="Imagine that you can modify a text with a few simple instructions, now you can achieve it."
+          />
+          <meta
+            property="og:image"
+            content="https://maketexteasy.com/og-image.png"
+          />
+          <meta
+            name="twitter:image"
+            content="https://maketexteasy.com/og-image.png"
+          />
+      </Head>
       <body>
         <Main />
         <NextScript />
